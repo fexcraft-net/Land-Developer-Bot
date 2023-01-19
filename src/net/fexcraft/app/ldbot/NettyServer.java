12a60d718646e3aa08264e0cf5e63115beaf8ca1
Ferdinand Calo <fercalo96@yahoo.de>
2023-01-19 21:46:03 +0100

    Update NettyServer.java

--- src/net/fexcraft/app/ldbot/NettyServer.java
+++ src/net/fexcraft/app/ldbot/NettyServer.java
@@ -1,25 +1,28 @@
 package net.fexcraft.app.ldbot;
-//http://www.mastertheboss.com/jboss-frameworks/netty/jboss-netty-tutorial/
 
 import java.util.ArrayList;
 import java.util.concurrent.ConcurrentHashMap;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandler.Sharable;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelPipeline;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
-import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.handler.codec.string.StringDecoder;
-import io.netty.handler.codec.string.StringEncoder;
 import net.fexcraft.app.json.JsonHandler;
 import net.fexcraft.app.json.JsonMap;
 
+/**
+ * 
+ * @author Ferdinand Calo' (FEX___96)
+ *
+ */
 public final class NettyServer {
 	
 	private static ChannelFuture fut;
@@ -29,15 +32,13 @@ public final class NettyServer {
 		EventLoopGroup work = new NioEventLoopGroup();
 		try {
 			ServerBootstrap boot = new ServerBootstrap();
-			boot.group(main, work).channel(NioServerSocketChannel.class).childHandler(new ChannelInitializer<SocketChannel>() {
+			boot.group(main, work).channel(NioServerSocketChannel.class).childHandler(new ChannelInitializer<SocketChannel>(){
 				@Override
 				public void initChannel(SocketChannel ch) throws Exception {
-					ChannelPipeline p = ch.pipeline();
-					p.addLast(new StringDecoder());
-					p.addLast(new StringEncoder());
-					p.addLast(new ServerHandler());
+					ch.pipeline().addLast(new MsgDecoder(), new MsgEncoder(), new ConnectionHandler());
 				}
-			});
+			}).option(ChannelOption.SO_BACKLOG, 128).childOption(ChannelOption.SO_KEEPALIVE, true);
+
 			fut = boot.bind(port).sync();
 			LandDevBot.log("Netty Server started.");
 			fut.channel().closeFuture().sync();
@@ -49,27 +50,28 @@ public final class NettyServer {
 	}
 
 	@Sharable
-	public static class ServerHandler extends SimpleChannelInboundHandler<String> {
+	public static class ConnectionHandler extends ChannelInboundHandlerAdapter {
 		
-		private ConcurrentHashMap<String, String> tokens = new ConcurrentHashMap<>();
+		private static ConcurrentHashMap<String, String> tokens = new ConcurrentHashMap<>();
 	
 		@Override
 		public void channelActive(final ChannelHandlerContext ctx) {
-			System.out.println("Client connected - " + ctx.channel().id());
+			LandDevBot.log("Client connected - " + ctx.channel().id());
+			LandDevBot.log("Clients connected so far: " + tokens.size());
 		}
 	
 		@Override
-		public void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
-			if(!msg.contains("=")) return;
-			String[] split = msg.split("=");
+		public void channelRead(ChannelHandlerContext ctx, Object obj) throws Exception {
+			Message msg = (Message)obj;
+			if(msg.length <= 0) return;
+			if(!msg.value.contains("=")) return;
+			String[] split = msg.value.split("=");
 			if(split[0].equals("token")){
 				String token = ctx.channel().remoteAddress().toString().split(":")[0] + ":" + split[1];
 				if(token.startsWith("/")) token = token.substring(1);
 				JsonMap map = LandDevBot.tokens();
 				if(map == null){
-					ctx.channel().writeAndFlush("token=invalid (" + token + ")");
-					ctx.channel().writeAndFlush("Server is not linked with the bot. 0");
-					ctx.close();
+					ctx.channel().writeAndFlush(new Message("token=invalid (" + token + ")\nServer is not linked with the bot. 0")).addListener(ChannelFutureListener.CLOSE);
 					return;
 				}
 				boolean found = false;
@@ -80,48 +82,47 @@ public final class NettyServer {
 					}
 				}
 				if(!found){
-					ctx.channel().writeAndFlush("token=invalid (" + token + ")");
-					ctx.channel().writeAndFlush("Server is not linked with the bot. 1");
-					ctx.close();
+					ctx.channel().writeAndFlush(new Message("token=invalid (" + token + ")\nServer is not linked with the bot. 1")).addListener(ChannelFutureListener.CLOSE);
 				}
 				else{
 					tokens.put(ctx.channel().id().toString(), token);
-					ctx.channel().writeAndFlush("OK");
+					ctx.channel().writeAndFlush(new Message(0));
 					LandDevBot.log("Connection created with '" + token + "'.");
 				}
 				return;
 			}
 			if(tokens.get(ctx.channel().id().toString()) == null) return;
 			if(split[0].equals("msg")){
+				LandDevBot.log(msg.value);
 				JsonMap map = JsonHandler.parse(split[1], true).asMap();
 				ArrayList<String> chan = LandDevBot.getChannel(tokens.get(ctx.channel().id().toString()), map.get("c").string_value());
 				if(chan == null){
-					ctx.channel().writeAndFlush("CHANNEL_NOT_LISTED");
+					ctx.channel().writeAndFlush(new Message("CHANNEL_NOT_LISTED"));
 				}
 				else{
 					String[] mesg = {  map.get("m").string_value() }; 
 					if(map.has("s")) mesg[0] = "**" + map.get("s").string_value() + "**: " + mesg[0];
 					for(String str : chan){
-						LandDevBot.api().getChannelById(str).get().asServerTextChannel().ifPresent(ch -> ch.sendMessage(mesg[0]));
+						//LandDevBot.api().getChannelById(str).get().asServerTextChannel().ifPresent(ch -> ch.sendMessage(mesg[0]));
 					}
-					ctx.channel().writeAndFlush("OK");
+					ctx.channel().writeAndFlush(new Message(0));
 				}
 			}
-			else ctx.channel().writeAndFlush("UNKNOWN_REQUEST");
+			else ctx.channel().writeAndFlush(new Message("UNKNOWN_REQUEST"));
 		}
 		
 		@Override
 		public void handlerRemoved(ChannelHandlerContext ctx){
-			System.out.println("Client disconnected - " + ctx.channel().id().toString());
+			LandDevBot.log("Client disconnected - " + ctx.channel().id().toString());
 			tokens.remove(ctx.channel().id().toString());
+			LandDevBot.log("Clients still connected: " + tokens.size());
 		}
 	
 		@Override
 		public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-			System.out.println("Client disconnected with error - " + ctx.channel().id().toString());
-			tokens.remove(ctx.channel().id().toString());
-			cause.printStackTrace();
-			ctx.close();
+			LandDevBot.log("Client errored - " + ctx.channel().id().toString());
+			//cause.printStackTrace();
+			//ctx.close();
 		}
 		
 	}
